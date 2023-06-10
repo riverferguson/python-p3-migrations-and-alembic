a095eb6514100b2a9443e0225ea83afdf5810187
River Ferguson <riverferguson@gmail.com>
2023-06-10 00:36:08 -0700

    complete lab

---
+++ lib/migrations/versions/8abea76fea0d_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 8abea76fea0d
+Revises: 
+Create Date: 2023-06-10 00:28:48.970337
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8abea76fea0d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
